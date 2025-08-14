acd394203a8a4bf8e1dedb06adb0c1eb333d3135
DivanshuGaur1 <divanshugaur13@gmail.com>
2025-08-14 11:29:51 +0530

    JS

---
+++ ECOMMERCE WEB/buyProduct.js
@@ -0,0 +1,11 @@
+const buyService=require('.../services/buyProductService');
+const buyProduct=async(req,res)=>{
+    try{
+        const productId=req.params.id;
+        buyService(productId);
+    }
+    catch(error){
+        console.error("Error in buyProduct:", error);
+        res.status(500).json(messsage:"Internal Server Error");
+    }
+}
---
+++ ECOMMERCE WEB/index.js
@@ -0,0 +1,10 @@
+const app=require('express');
+const app=app();
+const mongoose=require('mongoose');
+const cors=require();
+
+app.use(cors());
+app.use(app.json());
+app.get('/'(req,res)=>{
+
+})
